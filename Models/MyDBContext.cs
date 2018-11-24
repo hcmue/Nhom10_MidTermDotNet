6ede1aea0b617dfa00e999b736b805e4fc715d0a
MinhPhuc1998 <44628658+MinhPhuc1998@users.noreply.github.com>
2018-11-24 09:22:14 +0700

    Add files via upload

--- Models/MyDBContext.cs
+++ Models/MyDBContext.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Nhom10_MidTermDotNet.Models
 {
-    public class MyDBContext
+    public class MyDBContext : DbContext
     {
+        public DbSet<Khoa> Khoa { get; set; }
+        public DbSet<SinhVien> SinhVien { get; set; }
+        public DbSet<MonHoc> MonHoc { get; set; }
+        public DbSet<KetQua> KetQua { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        { optionsBuilder.UseSqlServer(@"Server=.;Database=QLSinhVien;Integrated Security=True;"); }
     }
 }
