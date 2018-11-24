9fa4bcdcdbbed20c54204e3aafaca9da293ea4f7
thanhlamtl <44388571+thanhlamtl@users.noreply.github.com>
2018-11-24 11:07:31 +0700

    Add files via upload

--- Models/Khoa.cs
+++ Models/Khoa.cs
@@ -1,19 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Web;
 
-namespace Nhom10_MidTermDotNet.Models
+namespace StudentManagement.Models
 {
     public class Khoa
     {
-        [Key]
-        [Display(Name = "Mã Khoa")]
-        public int MaKhoa { get; set; }
-        
-        [Display(Name = "Tên Khoa")]
-        public string TenKhoa { get; set; }
-        
+       
+            [Key]
+            [Display(Name = "Mã Khoa")]
+            public int MaKhoa { get; set; }
+
+            [Display(Name = "Tên Khoa")]
+            public string TenKhoa { get; set; }
+
+
+    }
+    class KhoaList
+    {
+        DBConnection db;
+        public KhoaList()
+        {
+            db = new DBConnection();
+        }
+
+        public object Khoalist { get; private set; }
+
+        public List<Khoa> getKhoa(string MaKhoa)
+        {
+            string sql;
+            if (string.IsNullOrEmpty(MaKhoa))
+                sql = "SELECT * FROM Khoas";
+            else
+                sql = "SELECT * FROM Khoas WHERE MaKhoa=" + MaKhoa;
+            List<Khoa> Khlist = new List<Khoa>();
+            DataTable dt = new DataTable();
+            SqlConnection con = db.GetConnection();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            con.Open();
+            da.Fill(dt);
+            da.Dispose();
+            con.Close();
+            Khoa tmpKhoa;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                tmpKhoa = new Khoa();
+                tmpKhoa.MaKhoa = Convert.ToInt32(dt.Rows[i]["MaKhoa"].ToString());
+                tmpKhoa.TenKhoa = dt.Rows[i]["TenKhoa"].ToString();
+  
+                Khlist.Add(tmpKhoa);
+            }
+            return Khlist;
+        }
     }
 }
---
+++ Models/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagement.Models
+{
+    public class Student
+    {
+        public int ID { set; get; }
+        [Required(ErrorMessage="Moi Nhap Ho va ten")]
+        [Display(Name="Ho va Ten:")]
+        public string FullName { set; get; }
+        [Required(ErrorMessage = "Moi Nhap Dia Chi")]
+        [Display(Name = "Dia Chi:")]
+        public string Address { set; get; }
+
+    }
+    class StudentList
+    {
+        DBConnection db;
+        public StudentList()
+        {
+            db = new DBConnection();
+        }
+        public List<Student> getStudent(string ID)
+        {
+            string sql;
+            if (string.IsNullOrEmpty(ID))
+                sql = "SELECT * FROM Students";
+            else
+                sql = "SELECT * FROM Students WHERE ID=" + ID;
+            List<Student> stulist = new List<Student>();
+            DataTable dt = new DataTable();
+            SqlConnection con = db.GetConnection();
+            SqlDataAdapter da = new SqlDataAdapter(sql, con);
+            con.Open();
+            da.Fill(dt);
+            da.Dispose();
+            con.Close();
+            Student tmpStu;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                tmpStu = new Student();
+                tmpStu.ID = Convert.ToInt32(dt.Rows[i]["ID"].ToString());
+                tmpStu.FullName = dt.Rows[i]["FullName"].ToString();
+                tmpStu.Address = dt.Rows[i]["Address"].ToString();
+                stulist.Add(tmpStu);
+            }
+            return stulist;
+        }
+    }
+}
