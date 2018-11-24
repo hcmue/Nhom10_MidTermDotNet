641f15bb5de43d9854330a9fcb530cf393da104a
MinhPhuc1998 <44628658+MinhPhuc1998@users.noreply.github.com>
2018-11-24 09:23:07 +0700

    Add files via upload

---
+++ Migrations/20181124021845_Nhom10_MidTermDotNet.QLSinhVien.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nhom10_MidTermDotNet.Migrations
+{
+    public partial class Nhom10_MidTermDotNetQLSinhVien : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KetQua",
+                columns: table => new
+                {
+                    MaKetQua = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MaMon = table.Column<int>(nullable: false),
+                    MaSV = table.Column<int>(nullable: false),
+                    Diem = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KetQua", x => x.MaKetQua);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Khoa",
+                columns: table => new
+                {
+                    MaKhoa = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TenKhoa = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Khoa", x => x.MaKhoa);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MonHoc",
+                columns: table => new
+                {
+                    MaMon = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TenMon = table.Column<string>(nullable: true),
+                    SoTinChi = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MonHoc", x => x.MaMon);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SinhVien",
+                columns: table => new
+                {
+                    MaSV = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    HoTen = table.Column<string>(maxLength: 100, nullable: true),
+                    NgaySinh = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SinhVien", x => x.MaSV);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "KetQua");
+
+            migrationBuilder.DropTable(
+                name: "Khoa");
+
+            migrationBuilder.DropTable(
+                name: "MonHoc");
+
+            migrationBuilder.DropTable(
+                name: "SinhVien");
+        }
+    }
+}
